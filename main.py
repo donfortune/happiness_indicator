0851a9819e0620d2c97700bf32acdf552723577e
donfortune <donfortunet.df@gmail.com>
2023-04-24 07:50:58 +0100

    added country functionality

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ import pandas as pd
 data = pd.read_csv('happy.csv')
 
 st.title('In Search For Happiness')
+country = st.text_input('Enter the name of your country:')
 xaxis = st.selectbox('Select the data for the X-axis', ('GDP', 'Happiness', 'Generosity'))
 yaxis = st.selectbox('Select the data for the Y-axis', ('GDP', 'Happiness', 'Generosity'))
 title = st.subheader(f'{xaxis} and {yaxis}')
 
 happy = data['happiness']
 gdps = data['gdp']
-if xaxis == 'GDP' and yaxis == 'Happiness':
+general = data['generosity']
+countrys = data['country']
+
+
+if xaxis == 'GDP' and yaxis == 'Happiness' and country in countrys.values:
+    filtered_data = data[data['country'] == country]
     figure = px.scatter(x=happy, y=gdps, labels={'x': 'happy', 'y': 'gdps'})
     st.plotly_chart(figure)
+elif xaxis == 'GDP' and yaxis == 'Generosity':
+    figure = px.scatter(x=general, y=gdps, labels={'x': 'general', 'y': 'gdps'})
+    st.plotly_chart(figure)
+#elif
+
 
